5960aa020e29bcae0fe9df57db9210e7750d56d0
Lisa Scott <lrscot@aol.com>
2019-02-12 03:34:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-hash 
+  smallest_person = {
+    :size => 1000
+  }
+  hash.each do |key, value|
+    if value < smallest_person[:size]
+      smallest_person[:size] = value
+      smallest_person[:name] = key
+    end
+  end
+smallest_person[:name]
 end
